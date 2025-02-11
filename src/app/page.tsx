efa59070094a7f0483eb4b8d47194cffda7789a9
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2025-02-11 14:18:11 -0600

    Shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "app/components/ui/button";
+
 export default function Home() {
-  return <h1>Hola mundo</h1>;
+  return (
+    <>
+      <h1>Hola mundo</h1>
+      <Button>Ratón</Button>
+    </>
+  );
 }
